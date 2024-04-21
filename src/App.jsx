91bbba1ca9cffd3077ff898e3caf085fee1732a6
Otavin96 <167653370+Otavin96@users.noreply.github.com>
2024-04-20 22:49:15 -0300

    Testando

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ function App() {
   return (
     <div className="container">
       <h1>Hello Bootstrap</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus velit odio iste, cupiditate neque quibusdam deserunt nam quos, magni facilis asperiores eius repellendus nihil saepe quasi ipsa quaerat eos modi?</p>
       <div className="btn btn-outline-primary">Cadastrar</div>
       <div className="btn btn-outline-success">Listar</div>
     </div>
