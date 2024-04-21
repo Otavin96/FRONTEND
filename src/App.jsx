77e2f1823df76de78880c476ae676f9b17bc411f
Otavin96 <167653370+Otavin96@users.noreply.github.com>
2024-04-20 22:30:29 -0300

    Enviando arquivos do primeiro curso para o rep

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <div className="container">
+      <h1>Hello Bootstrap</h1>
+      <div className="btn btn-outline-primary">Cadastrar</div>
+      <div className="btn btn-outline-success">Listar</div>
+    </div>
+  )
+}
+
+export default App
